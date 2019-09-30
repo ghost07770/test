945594bcc2dec9dd2fca1a96c64ef70aafc9c24d
ghost07770 <kritartha1990@gmail.com>
2019-09-30 20:57:29 +0530

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -1,26 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep 28 16:17:17 2019
+Created on Sun Sep 29 15:40:06 2019
 
 @author: Kritartha Patowary
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-#x co-ordinates
-x = np.arange(0, 9)
-A = np.array([x, np.ones(9)])
-
-#linearly generated sequence
-
-y = [19,20,20.5,21.5,22,23,23,25.5,24]
-#obtaining the parameters of regression line
-w = np.linalg.lstsq(A.T,y)[0]
-
-#plotting the line
-line = w[0]*x + w[1] #regression line
-plt.plot(x, line, 'r-')
-plt.hist(y)
-plt.plot(x, y, 'o')
-plt.show()
